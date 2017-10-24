628cb8ce5ed5fd42993dcec16508feddcf56e7a5
Ubuntu <ubuntu@ip-172-31-72-118.ec2.internal>
2017-10-24 19:43:08 +0000

    Quick cleanup

--- tar/tar_logs.sh
+++ tar/tar_logs.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# Copyright (c) 2017 All Rights Reserved, http://www.threatstack.com
-#current_date="$(date +'%m-%d-%Y')"
-#name=ts_logs_$(hostname)_${current_date}
-#tar -czvf ${name}.tar.gz -C /tmp/ threatstack_diagnostics || ( echo "Failed to generate tarball! Exiting" ; exit 1 )
 tar -czvf $1 -C /tmp/ threatstack_diagnostics || ( echo "Failed to generate tarball! Exiting" ; exit 1 )
