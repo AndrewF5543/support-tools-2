bce2a2dea062c364c4e8fe1812bab9872f49d066
Ubuntu <ubuntu@ip-172-31-72-118.ec2.internal>
2017-10-24 20:04:43 +0000

    Changinf the naming format on the directory in /tmp

--- diagnostics.sh
+++ diagnostics.sh
@@ -6,20 +6,20 @@ if [ $EUID -gt 0 ] ; then
     exit
 fi
 echo "/* BEGINNING THREAT STACK DIAGNOSTICS **********************************/"
-mkdir /tmp/threatstack_diagnostics
-bash system/gather_system_info.sh | sudo tee /tmp/threatstack_diagnostics/system_info.txt
-bash docker/gather_docker_info.sh | sudo tee /tmp/threatstack_diagnostics/docker_info.txt
-bash agent/gather_agent_info.sh | sudo tee /tmp/threatstack_diagnostics/agent_info.txt
-bash agent/gather_fim_info.sh | sudo tee /tmp/threatstack_diagnostics/fim_info.txt
-echo "/* Copying threat stack log files **************************************/"
-cp /opt/threatstack/cloudsight/logs/cloudsight.log /tmp/threatstack_diagnostics/cloudsight.log
-cp /opt/threatstack/cloudsight/logs/install.log /tmp/threatstack_diagnostics/install.log
-cp /opt/threatstack/cloudsight/logs/error.log /tmp/threatstack_diagnostics/error.log
-cp /opt/threatstack/cloudsight/logs/threatstack-audit.log /tmp/threatstack_diagnostics/threatstack-audit.log
-cp /opt/threatstack/cloudsight/logs/threatstack-tsfim.log /tmp/threatstack_diagnostics/threatstack-tsfim.log
-echo "/* Tarring log files ***************************************************/"
 current_date="$(date +'%m-%d-%Y')"
 name=ts_logs_$(hostname)_${current_date}.tar.gz
+mkdir /tmp/$name
+bash system/gather_system_info.sh | sudo tee /tmp/$name/system_info.txt
+bash docker/gather_docker_info.sh | sudo tee /tmp/$name/docker_info.txt
+bash agent/gather_agent_info.sh | sudo tee /tmp/$name/agent_info.txt
+bash agent/gather_fim_info.sh | sudo tee /tmp/$name/fim_info.txt
+echo "/* Copying threat stack log files **************************************/"
+cp /opt/threatstack/cloudsight/logs/cloudsight.log /tmp/$name/cloudsight.log
+cp /opt/threatstack/cloudsight/logs/install.log /tmp/$name/install.log
+cp /opt/threatstack/cloudsight/logs/error.log /tmp/$name/error.log
+cp /opt/threatstack/cloudsight/logs/threatstack-audit.log /tmp/$name/threatstack-audit.log
+cp /opt/threatstack/cloudsight/logs/threatstack-tsfim.log /tmp/$name/threatstack-tsfim.log
+echo "/* Tarring log files ***************************************************/"
 ( bash tar/tar_logs.sh $name ) || exit 1
 echo "/* Done tarring log files **********************************************/"
 security_setting=${1:-"--send-safe"}
@@ -27,6 +27,6 @@ if [ $security_setting != "--send-unsafe"  ] ; then
     gpg --import support.pub
     gpg --encrypt --trust-model always -r support@threatstack.com $name
 fi
-rm -rf /tmp/threatstack_diagnostics 
+rm -rf /tmp/$name 
 echo "/* ENDING THREAT STACK DIAGNOSTICS *************************************/"
 
