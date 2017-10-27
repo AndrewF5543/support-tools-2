1f185e920b7d2d8b783fc45096b2d3ed1e7a5a3e
Ubuntu <ubuntu@ip-172-31-72-118.ec2.internal>
2017-10-27 20:08:23 +0000

    Removed unnecessary sudos, imrpoved GPG behavior, exiting 1 on non-root usage

--- diagnostics.sh
+++ diagnostics.sh
@@ -3,16 +3,16 @@
 # Copyright (c) 2017 All Rights Reserved, http://www.threatstack.com
 if [ $EUID -gt 0 ] ; then
     echo "You must be root to run diagnostics.sh"
-    exit
+    exit 1
 fi
 echo "/* BEGINNING THREAT STACK DIAGNOSTICS **********************************/"
 current_date="$(date +'%m-%d-%Y')"
 name=ts_logs_$(hostname)_${current_date}
 mkdir /tmp/$name
-bash system/gather_system_info.sh | sudo tee /tmp/$name/system_info.txt
-bash docker/gather_docker_info.sh | sudo tee /tmp/$name/docker_info.txt
-bash agent/gather_agent_info.sh | sudo tee /tmp/$name/agent_info.txt
-bash agent/gather_fim_info.sh | sudo tee /tmp/$name/fim_info.txt
+bash system/gather_system_info.sh | tee /tmp/$name/system_info.txt
+bash docker/gather_docker_info.sh | tee /tmp/$name/docker_info.txt
+bash agent/gather_agent_info.sh | tee /tmp/$name/agent_info.txt
+bash agent/gather_fim_info.sh | tee /tmp/$name/fim_info.txt
 echo "/* Copying threat stack log files **************************************/"
 cp /opt/threatstack/cloudsight/logs/cloudsight.log /tmp/$name/cloudsight.log
 cp /opt/threatstack/cloudsight/logs/install.log /tmp/$name/install.log
@@ -26,7 +26,7 @@ echo "/* Done tarring log files **********************************************/"
 security_setting=${1:-"--send-safe"}
 if [ $security_setting != "--send-unsafe"  ] ; then
     gpg --import support.pub
-    gpg --encrypt --trust-model always -r support@threatstack.com $name.tar.gz
+    gpg --encrypt -r support@threatstack.com $name.tar.gz
 fi
 rm -rf /tmp/$name 
 echo "/* ENDING THREAT STACK DIAGNOSTICS *************************************/"
